b59a43cd53b9bba20bbdef3360a275396bba09f0
Khosuke <9814@holbertonstudents.com>
2024-12-13 10:24:33 +0100

    Changes to comply to betty coding style and documentation

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -1,6 +1,13 @@
 #include <stddef.h>
 #include "sort.h"
 
+/**
+ * _swap_ - swap two integers
+ * @a: first element
+ * @b: second element
+ *
+ * Return: no return
+ */
 void _swap_(int *a, int *b)
 {
 	int temp;
@@ -10,12 +17,19 @@ void _swap_(int *a, int *b)
 	*b = temp;
 }
 
+/**
+ * selection_sort - sorts an array of integers in ascending
+ * order using the Selection sort algorithm
+ * @array: array to sort
+ * @size: size of the array
+ *
+ * Return: no return
+ */
 void selection_sort(int *array, size_t size)
 {
-	size_t i,j;
+	size_t i, j;
 
 	i = 0;
-
 	if (array == NULL || size < 2)
 		return;
 
